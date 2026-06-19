b0504abb583b168fbd4f09eb360355cd4c120123
Nasir Ahmad Ehsan <nasirahmadehsan1383@gmail.com>
2026-06-20 00:04:09 +0430

    feat: setup and verify Tailwind CSS in frontend

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,7 +1,17 @@
 function App() {
   return (
-    <div>
-      <h1>Your E-Commerce Client</h1>
+    <div className="min-h-screen bg-gray-100 flex flex-col items-center justify-center">
+      <div className="bg-white p-8 rounded-xl shadow-md text-center">
+        <h1 className="text-3xl font-bold text-blue-600 mb-2">
+          Your E-Commerce
+        </h1>
+        <p className="text-gray-600">
+          Wellcome to Your E-Commerce
+        </p>
+        <button className="mt-4 px-6 py-2 bg-blue-500 text-white font-semibold rounded-lg hover:bg-blue-600 transition">
+          Cehck Button
+        </button>
+      </div>
     </div>
   )
 }
