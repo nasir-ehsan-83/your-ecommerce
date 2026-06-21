b9a84e534f8105c487bbe44e94d8423ea48d5f96
Nasir Ahmad Ehsan <nasirahmadehsan1383@gmail.com>
2026-06-21 20:52:35 +0430

    add add name property to registerSchema

--- server/src/validators/auth.validator.js
+++ server/src/validators/auth.validator.js
@@ -2,8 +2,9 @@ import z from "zod";
 
 export const registerSchema = z.object({
     body: z.object({
-        username: z.string().min(3, "Username length should not be less than 3"),
-        email: z.string().email("Invalid email format"),
+        name: z.string().min(3, "Name's length should not be less than 3"),
+        username: z.string().min(3, "Username's length should not be less than 3"),
+        email: z.string().email("Invalid email format").min(10),
         password: z.string().min(8, "Password length should not be less than 8")
     })
 });
