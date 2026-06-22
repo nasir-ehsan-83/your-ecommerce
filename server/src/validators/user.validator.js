a4e6068ca631d177054688f8512843c22a597edc
Nasir Ahmad Ehsan <nasirahmadehsan1383@gmail.com>
2026-06-22 16:46:52 +0430

    feat: transform registerScehma from auth.validator.js to user.validator.js as createUserSchema

--- server/src/validators/user.validator.js
+++ server/src/validators/user.validator.js
@@ -1,8 +1,10 @@
 import z from "zod";
 
-export const loginSchema = z.object({
+export const createUserSchema = z.object({
     body: z.strictObject({
+        name: z.string().min(3, "Name's length should not be less than 3"),
         username: z.string().min(3, "Username's length should not be less than 3"),
+        email: z.string().email("Invalid email format").min(10),
         password: z.string().min(8, "Password length should not be less than 8")
     })
 });
