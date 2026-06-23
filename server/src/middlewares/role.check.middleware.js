3746e69cc8892e1898560785acd262877e53cd9a
Nasir Ahmad Ehsan <nasirahmadehsan1383@gmail.com>
2026-06-23 21:33:05 +0430

    feat(auth): add checkRole middleware for RBAC

---
+++ server/src/middlewares/role.check.middleware.js
@@ -0,0 +1,17 @@
+import { asyncHandler } from "../utils/async.handler";
+
+export const checkRole = (allowedRoles) => {
+    // function to check user.role
+    return asyncHandler(async (req, res, next) => {
+        
+        // is user.role in allowdRoles?
+        if (!allowedRoles.includes(req.user.role)) {
+            return res.status(403).json({
+                status: "error",
+                message: "Forbidden: You do not have permission to access this resource"
+            });
+        }
+        
+        next();
+    });
+};
