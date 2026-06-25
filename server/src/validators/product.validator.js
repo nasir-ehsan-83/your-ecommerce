1800ddebba0d134a3a86f107f2a54ff066e106ef
Nasir Ahmad Ehsan <nasirahmadehsan1383@gmail.com>
2026-06-25 12:30:36 +0430

    feat(product): add productResponseSchema in validators/product.validator.js

--- server/src/validators/product.validator.js
+++ server/src/validators/product.validator.js
@@ -10,3 +10,21 @@ export const createProductSchema = z.object({
         stock: z.number().partial()
     })
 });
+
+export const productResponseSchema = z.object({
+    _id: z.any(), // to get mongo's ObjectId
+    name: z.string(),
+    description: z.string(),
+    price: z.number(),
+    category: z.string(),
+    imageURL: z.string(),
+    stock: z.number()
+}).transform((data) => ({
+    id: data._id.toString(), // convert mongo's ObjectId to string
+    name: data.name,
+    description: data.description,
+    price: data.price,
+    category: data.category,
+    imageURL: data.imageURL,
+    stock: data.stock
+}));
