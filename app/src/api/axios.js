fdb7ca5507d6fefee50f4998e07ec16c8fd0cce5
Nasir Ahmad Ehsan <nasirahmadehsan1383@gmail.com>
2026-06-28 16:27:08 +0430

    refactor(axios-api): update 'axios' configuration

--- app/src/api/axios.js
+++ app/src/api/axios.js
@@ -1,5 +1,19 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL: "http://localhost:3500s/api"
+  baseURL: "http://localhost:3500/api", 
+  withCredentials: true 
 });
+
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("accessToken");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
