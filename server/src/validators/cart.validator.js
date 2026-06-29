2c68edc5a6cf88c0a8255b43524a8f159f0e3a02
Nasir Ahmad Ehsan <nasirahmadehsan1383@gmail.com>
2026-06-29 22:35:59 +0430

    feat(validation): add 'cartResponse' validator for response validation

--- server/src/validators/cart.validator.js
+++ server/src/validators/cart.validator.js
@@ -1,19 +1,35 @@
-import mongoose from "mongoose";
 import z from "zod";
+import { objectIdSchema } from "./user.validator.js";
 
 export const addToCartSchema = z.object({
     body: z.strictObject({
-        userId: z.string().refine(
-            (val) => mongoose.Types.ObjectId(val),
-            {
-                message: "Invalid User ID Format"
-            }
-        ),
-        productId: z.string().refine(
-            (val) => mongoose.Types.ObjectId(val),
-            {
-                message: "Invalid Product ID Format"
-            }
-        )
+        productId: objectIdSchema,
+        quantity: z.number().optional()
     })
 });
+
+export const cartResponse = z.strictObject({
+    message: z.string,
+    cart: z.object({
+        _id: objectIdSchema,
+        userId: objectIdSchema,
+        items: z.array(
+            z.object({
+            _id: objectIdSchema,
+            prodectId: z.any(),
+            quantity: z.number(),
+        })
+        )
+    })
+}).strict().transform((data) => ({
+    message: data.message,
+    cart: {
+        id: data.cart._id.toString(),
+        userId: data.cart.userId.toString(),
+        items: data.cart.items.map((item) => ({
+            id: item._id.toString(),
+            productId: item.prodectId.toString(),
+            quantity: item.quantity
+        } ))
+    }
+}));
